6f522c9edc0c811eef7e149b7f913bb545c4a8a1
maxin <mx1759808857@163.com>
2018-04-12 23:30:34 +0800

    网络连接

---
+++ Server/Assets/Scripts/NetworkClient.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Net;
+
+public class NetworkClient : MonoBehaviour
+{
+
+    void Start()
+    {
+        ClientSocket mSocket = new ClientSocket();
+        mSocket.ConnectServer("127.0.0.1", 8088);
+        mSocket.SendMessage("服务器傻逼！");
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+
+    }
+}
